db3d18459bcc6edaf9f0f1f371fc89893e5ee592
CharlesMataOfficial <charleseyes2002@gmail.com>
2025-11-08 13:34:19 +0800

    Fix DEBUG capitalization

--- smartday_project/settings.py
+++ smartday_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret-for-local")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
