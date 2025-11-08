db494349cc7899ebf1d1a700c440623dcccad2e1
CharlesMataOfficial <charleseyes2002@gmail.com>
2025-11-08 13:38:29 +0800

    fix allowed host

--- smartday_project/settings.py
+++ smartday_project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret-for-local")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["smartday-personal-productivity-tracker.onrender.com",
+    "localhost",]
 
 
 # Application definition
